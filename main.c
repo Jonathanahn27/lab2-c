65f451d5b1e9ea9aac316c760c09e7a248e82dd5
Jonathanahn27 <Jonathanahn27@gmail.com>
2020-09-09 02:11:24 +0000

    space

--- main.c
+++ main.c
@@ -23,19 +23,19 @@ int main(void) {
   else if (grade >= 83.0) {
     printf("Your letter grade for CMPSC 131 is B.\n");
   }
-  else if (grade >=80.0) {
+  else if (grade >= 80.0) {
     printf("Your letter grade for CMPSC 131 is B-.\n");
   }
-  else if (grade >=77.0) {
+  else if (grade >= 77.0) {
     printf("Your letter grade for CMPSC 131 is C+.\n");
   }
-  else if (grade >=70.0) {
+  else if (grade >= 70.0) {
     printf("Your letter grade for CMPSC 131 is C.\n");
   }
-  else if (grade >=60.0) {
+  else if (grade >= 60.0) {
     printf("Your letter grade for CMPSC 131 is D.\n");
   }
-  else if (grade <60.0) {
+  else if (grade < 60.0) {
     printf("Your letter grade for CMPSC 131 is F.\n");
   }
 
